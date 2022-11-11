aa8b82ce40d593d2c07b2a8874df99ca249fb4a5
blin04 <ivanbokor17@gmail.com>
2022-11-11 17:05:56 +0100

    tokens are now represented as object of a class, lexer has also been turned into a class

--- lexer.py
+++ lexer.py
@@ -1,64 +1,82 @@
-"""
-Task of the lexer is to divide expression into tokens,
-following some grammar
-
-GRAMMAR:
-
-old grammar
-<izraz> = <broj> | <mnozenje> | <sabiranje>
-sabiranje = <mnozenje> ( <operator_sabiranja> <mnozenje> )*
-mnozenje = <atom> ( <operator_mnozenja> <atom> )*
-<atom> = <broj> | <izraz_u_zagradi>
-<izraz_u_zagradi> = <izraz>
-"""
-# TODO: make lexer class
 DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
-def skip_blank(text):
-
-    result = ""
-    for t in text:
-        if t != " ":
-            result += t
-
-    return result
+def is_digit(char):
+    if char in DIGITS:
+        return True
+    return False
 
 
-def tokenize(expr):
-    size = len(expr)
-    tokens = []
+class Token:
 
-    expr = skip_blank(expr)
+    def __init__(self, token, value):
+        self.type = token
+        self.value = value
 
-    # going through expression and
-    # recognizing tokens
-
-    i = 0
-    while i < size:
-        if expr[i] in DIGITS and expr[i] != '0':
-            tokens.append("NUMBER")
-
-            while i + 1 < size and expr[i + 1] in DIGITS:
-                i += 1
-
-        elif expr[i] == "*":
-            tokens.append("MUL")
-
-        elif expr[i] == "/":
-            tokens.append("DIV")
-
-        elif expr[i] == "+":
-            tokens.append("ADD")
-
-        elif expr[i] == "-":
-            tokens.append("SUB")
-        else:
-            print("found invalid token during tokenization")
-            return
-
-        i += 1
-
-    return tokens
+    def __str__(self):
+        return self.type + " " + str(self.value)
 
 
+class Lexer:
+
+    def __init__(self, text):
+        self.text = text
+        self.tokens = []
+
+    def skip_blank(self):
+        compressed_text = ""
+        for char in self.text:
+            if char != " ":
+                compressed_text += char
+
+        self.text = compressed_text
+
+    def tokenize(self):
+        self.skip_blank()
+        size = len(self.text)
+
+        i = 0
+        while i < size:
+            token = Token("CURRENT", -1)
+
+            if is_digit(self.text[i]) and self.text[i] != '0':
+                value = int(self.text[i])
+
+                while i + 1 < size and is_digit(self.text[i + 1]):
+                    value *= 10
+                    value += int(self.text[i + 1])
+                    i += 1
+
+                token.type = "NUMBER"
+                token.value = value
+
+            elif self.text[i] == '(':
+                token.type = "LPAR"
+                token.value = '('
+
+            elif self.text[i] == ')':
+                token.type = "RPAR"
+                token.value = ')'
+
+            elif self.text[i] == "+":
+                token.type = "ADD"
+                token.value = '+'
+
+            elif self.text[i] == "-":
+                token.type = "SUB"
+                token.value = '-'
+
+            elif self.text[i] == "*":
+                token.type = "MUL"
+                token.value = '*'
+
+            elif self.text[i] == "/":
+                token.type = "DIV"
+                token.value = '/'
+            else:
+                print("LexerError: unrecognized symbol")
+                return
+
+            self.tokens.append(token)
+            i += 1
+        return
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-"""
-Calculator implementation in python
 
-tokens = lexer(expression)
-tree = parser(tokens)
-evaluate(tree)
+# TODO: 1. fix parser, 2. choose what tree to construct (parse or ast), 3. create class for tree
 
-"""
-from lexer import tokenize
+from lexer import Lexer
 from parser import Parser
 
 if __name__ == '__main__':
     expression = input()
 
-    tokens = tokenize(expression)
-    parser = Parser(tokens)
+    lexer = Lexer(expression)
+    lexer.tokenize()
 
-    parser.printTokens()
+    print(len(lexer.tokens))
 
-    print(tokens)
+    for t in lexer.tokens:
+        print(t)
